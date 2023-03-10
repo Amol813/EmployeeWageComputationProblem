414956bf05cefbb08f7e902f1ad40ce47da0ed9a
unknown <amoljadhav>
2023-03-10 14:30:39 +0530

    [amol]add:EmployeeWageComputationProblem

---
+++ EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Employee Wage Program!");
+        }
+    }
+}
