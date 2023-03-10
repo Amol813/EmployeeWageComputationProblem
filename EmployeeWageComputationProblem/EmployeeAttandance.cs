06a8b93a960af9780f326e1406045be144fc17d5
unknown <amoljadhav>
2023-03-10 15:31:22 +0530

    [amol]add:SwitchCase

--- EmployeeWageComputationProblem/EmployeeAttandance.cs
+++ EmployeeWageComputationProblem/EmployeeAttandance.cs
@@ -4,9 +4,7 @@ namespace EmployeeWageComputationProblem
 {
     public class EmployeeAttandance
     {
-
         const int IS_PRESENT = 1, WAGE_PER_HR = 20, NO_OF_HRS = 8, IS_FULL_TIME = 1, IS_PART_TIME = 2;
-
         public void attandance()
         {
             Random random = new Random();
@@ -21,15 +19,17 @@ namespace EmployeeWageComputationProblem
             int wage = 0;
             Random random = new Random();
             int check = random.Next(0, 3);
-            if (check == IS_FULL_TIME)
-                wage = WAGE_PER_HR * NO_OF_HRS;
-            if (check == IS_PART_TIME)
-                wage = WAGE_PER_HR * (NO_OF_HRS / 2);
+            switch (check)
+            {
+                case IS_FULL_TIME:
+                    wage = NO_OF_HRS * WAGE_PER_HR;
+                    break;
+                case IS_PART_TIME:
+                    wage = WAGE_PER_HR * (NO_OF_HRS / 2);
+                    break;
+            }
             Console.WriteLine("Daily wage of an employee is: " + wage);
-
-
         }
-
 
     }
 }
