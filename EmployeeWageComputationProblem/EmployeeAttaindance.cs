ca72c06a9b8d9ba6823ded0c18f8078a08827842
unknown <amoljadhav>
2023-03-10 14:48:19 +0530

    [amol]add:EmployeeAttandance

---
+++ EmployeeWageComputationProblem/EmployeeAttaindance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem
+{
+    public class EmployeeAttaindance
+    {
+        const int IS_PRESENT = 1;
+        public void attandance()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+
+    }
+}
--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Employee Wage Program!");
+            EmployeeAttaindance EmployeeAttaindance = new EmployeeAttaindance();
+            EmployeeAttaindance.attandance();
         }
     }
 }
